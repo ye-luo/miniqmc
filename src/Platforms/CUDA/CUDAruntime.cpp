37ad711e0281bcce41029c69fc3c1508b750e904
Ye Luo <yeluo@anl.gov>
2026-04-13 14:12:38 -0500

    Update CUDAruntime include order

--- src/Platforms/CUDA/CUDAruntime.cpp
+++ src/Platforms/CUDA/CUDAruntime.cpp
@@ -11,6 +11,23 @@
 
 
 #include "CUDAruntime.hpp"
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+
+void cudaAssert(cudaError_t code, const std::string& cause, const char* filename, int line, bool abort)
+{
+  if (code != cudaSuccess)
+  {
+    std::ostringstream err;
+    err << "cudaAssert: " << cudaGetErrorName(code) << " " << cudaGetErrorString(code) << ", file " << filename
+        << ", line " << line << std::endl
+        << cause << std::endl;
+    std::cerr << err.str();
+    if (abort)
+      throw std::runtime_error(cause);
+  }
+}
 
 size_t getCUDAdeviceFreeMem()
 {
